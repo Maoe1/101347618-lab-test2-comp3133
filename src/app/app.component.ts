104ccdfb3d054b812289d86d87f519d5a64cfba0
Mahmoud Farghali <101347618@georgebrown.ca>
2023-04-02 23:10:38 -0400

    modules added/ api fetched from restpoint

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MissionListComponent } from './mission-list/mission-list.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
